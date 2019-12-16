6c0c2bc9205ab5e5046e624cedb9e29af827b038
brad <brad@brad.tw>
2019-12-16 21:30:14 +0800

    2

---
+++ app/src/main/java/tw/org/iii/android201915/MyService.java
@@ -0,0 +1,29 @@
+package tw.org.iii.android201915;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
