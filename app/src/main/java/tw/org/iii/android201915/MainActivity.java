38484a1c38d4430207398639c3a2124270d374d2
brad <brad@brad.tw>
2019-12-18 19:36:23 +0800

    8

--- app/src/main/java/tw/org/iii/android201915/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201915/MainActivity.java
@@ -2,19 +2,40 @@ package tw.org.iii.android201915;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
+    private MyReceiver myReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         seekBar = findViewById(R.id.seekbar);
+        myReceiver = new MyReceiver();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter("ACTION_BRAD");
+        registerReceiver(myReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(myReceiver);
     }
 
     public void playMusic(View view) {
@@ -33,4 +54,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MyService.class);
         stopService(intent);
     }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len", -1);
+            if (len >= 0){
+                seekBar.setMax(len);
+            }
+        }
+    }
+
 }
