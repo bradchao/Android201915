a74f7bddc04eb31b3040ca988e5ade9eef798fb9
brad <brad@brad.tw>
2019-12-18 19:00:20 +0800

    4

--- app/src/main/java/tw/org/iii/android201915/MyService.java
+++ app/src/main/java/tw/org/iii/android201915/MyService.java
@@ -2,10 +2,14 @@ package tw.org.iii.android201915;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+    private MediaPlayer mediaPlayer;
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
@@ -16,18 +20,24 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.v("brad", "onCreate");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.ear);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.v("brad", "onStartCommand");
+
+        mediaPlayer.start();
+
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.v("brad", "onDestroy");
+        mediaPlayer.release();
     }
 }
